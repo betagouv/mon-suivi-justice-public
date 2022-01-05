4bcc30bda2a46bd919ba35e8337b7f3acd4dcd43
Damien Le Thiec <damien.lethiec@gmail.com>
2022-01-05 21:50:28 +0100

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,24 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#landing'
+
+  scope controller: :static_pages do
+    get :landing
+    get :comprendre_ma_peine
+    get :regles_essentielles
+    get :obligations_personnelles
+    get :sursis_probatoire
+    get :travail_interet_general
+    get :suivi_socio_judiciaire
+    get :stage
+    get :amenagements_de_peine
+    get :preparer_mon_rdv
+    get :preparer_spip92
+    get :preparer_sap_nanterre
+    get :preparer_spip28
+    get :preparer_sap_chartres
+    get :ma_reinsertion
+    get :donnees_personnelles
+  end
 end
