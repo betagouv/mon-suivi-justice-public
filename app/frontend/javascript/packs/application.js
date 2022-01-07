529223b338eb2bfde43f73e1648494fb13526a2d
Damien Le Thiec <damien.lethiec@gmail.com>
2022-01-07 14:14:40 +0100

    Add sass reloader

--- app/frontend/javascript/packs/application.js
+++ app/frontend/javascript/packs/application.js
@@ -3,7 +3,7 @@ import 'controllers'
 import 'static'
 import "@gouvfr/dsfr/dist/dsfr/dsfr.css"
 import "@gouvfr/dsfr/dist/dsfr/dsfr.module.min.js"
-import "../../stylesheets/application.sass.scss"
+import "../../stylesheets/application.scss"
 
 // Allow to use image_pack_tag with images in frontend folder
 require.context('../../images', true)
