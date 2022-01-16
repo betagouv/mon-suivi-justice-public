1b5f910e3e34731195f63d185e3abc06835ed1ac
Damien Le Thiec <damien.lethiec@gmail.com>
2022-01-16 12:09:05 +0100

    Add page stage

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -59,6 +59,7 @@ Spina::Theme.register do |theme|
   theme.view_templates = [
     {name: "landing", title: "Landing", parts: %w[main_title card1_text card1_button_head card1_button_sub card2_text card2_button_head card2_button_sub card3_text card3_button_head card3_button_sub main_description]},
     {name: "amenagements_de_peine", title: "Aménagements de peine", parts: %w[main_title main_rich_description rich_collapse]},
+    {name: "stage", title: "Le stage", parts: %w[main_title rich_collapse]},
     {name: "comprendre_ma_peine", title: "Comprendre ma peine", parts: %w[main_title main_description first_subtitle card1_title card1_text card2_title card2_text secondary_subtitle basic_card]},
     {name: "donnees_personnelles", title: "Données personnelles", parts: %w[main_title main_rich_description]},
     {name: "ma_reinsertion", title: "Investir ma réinsertion", parts: %w[main_title subtitle main_rich_description button_text]},
@@ -73,6 +74,7 @@ Spina::Theme.register do |theme|
   theme.custom_pages = [
     {name: "landing", title: "Page d'accueil", view_template: "landing"},
     {name: "amenagements_de_peine", title: "Aménagements de peine", view_template: "amenagements_de_peine"},
+    {name: "stage", title: "Le stage", view_template: "stage"},
     {name: "comprendre_ma_peine", title: "Comprendre ma peine", view_template: "comprendre_ma_peine"},
     {name: "donnees_personnelles", title: "Données personnelles", view_template: "donnees_personnelles"},
     {name: "ma_reinsertion", title: "Investir ma réinsertion", view_template: "ma_reinsertion"},
