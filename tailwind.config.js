733f1c54b7af79d9a38ccec6aadc65106a182e5d
Damien Le Thiec <damien.lethiec@gmail.com>
2022-03-09 08:37:15 +0100

    Setup tailwindcss (basic)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: [
+      './app/views/**/*.html.erb',
+      './app/helpers/**/*rb',
+      './app/javascript/**/*.js'
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
