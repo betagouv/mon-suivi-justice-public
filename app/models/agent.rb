e0f45f17a74e958d87244fd76cc5479baa73a1d8
Damien Le Thiec <damien.lethiec@gmail.com>
2022-03-16 01:34:51 +0100

    Fix standardrb

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,7 +2,7 @@ class Agent
   include ActiveModel::Model
 
   attr_accessor :id, :first_name, :last_name, :phone, :email,
-                :organization_name, :share_info_to_convict
+    :organization_name, :share_info_to_convict
 
   def full_name
     "#{first_name} #{last_name}".titleize
