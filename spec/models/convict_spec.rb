395b5a116f76158c4d3aa54e8e45a2f1ee68634b
Damien Le Thiec <damien.lethiec@gmail.com>
2022-04-08 16:28:13 +0200

    Add password validation

--- spec/models/convict_spec.rb
+++ spec/models/convict_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Convict, type: :model do
     it { should validate_uniqueness_of(:msj_id) }
     it { should validate_presence_of(:phone) }
     it { should validate_uniqueness_of(:phone).case_insensitive }
+
+    it 'should validate password format' do
+      convict = Convict.new(password: "example")
+      expect(convict).to be_invalid
+      expect(convict.errors.messages[:password]).to include("est trop court (au moins 10 caractères)")
+      expect(convict.errors.messages[:password]).to include("doit contenir au moins un chiffre")
+      expect(convict.errors.messages[:password]).to include("doit contenir au moins un signe de ponctuation")
+      expect(convict.errors.messages[:password]).to include("doit contenir au moins une lettre majuscule")
+    end
   end
 end
