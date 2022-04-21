4c26466d072a40f56810a5616d049d02ed96587b
Damien Le Thiec <damien.lethiec@gmail.com>
2022-04-21 16:36:48 +0200

    Add human name place in view

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
   attr_accessor :id, :datetime, :duration, :state, :origin_department, :place,
     :organization_name, :agenda_name, :appointment_type, :display_address
 
-  delegate :name, :address, :phone, :email, :contact_method, :contact, :map_link,
+  delegate :name, :human_name, :address, :phone, :email, :contact_method, :contact, :map_link,
     to: :place, prefix: true
 
   def days_from_now
