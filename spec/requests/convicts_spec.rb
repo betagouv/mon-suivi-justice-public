c926d449d4898bc019ec5e3dae491e8ad22d6357
Damien Le Thiec <damien.lethiec@gmail.com>
2022-05-13 17:40:01 +0200

    Add tests

---
+++ spec/requests/convicts_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Sms Invitation", type: :request do
+  let(:headers) do
+    {Authorization: ActionController::HttpAuthentication::Basic.encode_credentials("username", "password")}
+  end
+
+  describe "DELETE /destroy" do
+    let(:path) { '/convicts/1' }
+    let(:do_request) { delete(path.to_s, headers:) }
+
+    before do
+      FactoryBot.create(:convict, msj_id: 1)
+    end
+
+    it "destroy the convict" do
+      expect { do_request }.to change(Convict, :count).by(-1)
+    end
+  end
+end
