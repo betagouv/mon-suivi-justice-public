c9275fa6191718b3e205b9722f4048b76c9ad255
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-06-24 16:49:43 +0200

    fix typo

--- spec/jobs/sms_sender_job_spec.rb
+++ spec/jobs/sms_sender_job_spec.rb
@@ -1,26 +1,24 @@
 require "rails_helper"
 
 RSpec.describe SmsSenderJob, type: :job do
-  context "with a phone number" do
-    let(:convict) { FactoryBot.create :convict, phone: "+33606060607" }
-    let(:content) { "test SMS" }
+  let(:convict) { FactoryBot.create :convict, phone: "+33606060607" }
+  let(:content) { "test SMS" }
 
-    describe "#perform" do
-      let(:tested_method) { SmsSenderJob.perform_now(convict, content) }
-      let(:adapter_dbl) { instance_double LinkMobilityAdapter, send_sms: true }
+  describe "#perform" do
+    let(:tested_method) { SmsSenderJob.perform_now(convict, content) }
+    let(:adapter_dbl) { instance_double LinkMobilityAdapter, send_sms: true }
 
-      before do
-        allow(LinkMobilityAdapter).to receive(:new).and_return adapter_dbl
-        tested_method
-      end
+    before do
+      allow(LinkMobilityAdapter).to receive(:new).and_return adapter_dbl
+      tested_method
+    end
 
-      it "instantiates LinkMobilityAdapter" do
-        expect(LinkMobilityAdapter).to have_received(:new).once
-      end
+    it "instantiates LinkMobilityAdapter" do
+      expect(LinkMobilityAdapter).to have_received(:new).once
+    end
 
-      it "send sms" do
-        expect(adapter_dbl).to have_received(:send_sms).once.with(convict, content)
-      end
+    it "send sms" do
+      expect(adapter_dbl).to have_received(:send_sms).once.with(convict, content)
     end
   end
 end
