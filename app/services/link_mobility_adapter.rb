3a026fe4f18c22e179f3e4c3bf6bc22932921795
Rémy Maucourt <remy.maucourt@yahoo.fr>
2022-06-24 16:46:56 +0200

    fix linter

--- app/services/link_mobility_adapter.rb
+++ app/services/link_mobility_adapter.rb
@@ -2,17 +2,17 @@
 class LinkMobilityAdapter
   def initialize
     @client = Faraday.new(
-      url: 'https://europe.ipx.com'
+      url: "https://europe.ipx.com"
     ) do |conn|
-      conn.request :authorization, :basic, ENV['LM_ACCOUNT'], ENV['LM_PWD']
+      conn.request :authorization, :basic, ENV["LM_ACCOUNT"], ENV["LM_PWD"]
     end
   end
 
   def send_sms(convict, content)
     sms_data = format_data(convict, content)
 
-    response = @client.post('/restapi/v1/sms/send') do |req|
-      req.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+    response = @client.post("/restapi/v1/sms/send") do |req|
+      req.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
       req.body = URI.encode_www_form(sms_data)
     end
 
@@ -32,14 +32,14 @@ class LinkMobilityAdapter
   def save_text_message(convict, content, response)
     response_hash = JSON.parse(response.body)
 
-    if (response_hash['responseCode']).zero?
+    if (response_hash["responseCode"]).zero?
       TextMessage.create(
-        message_id: response_hash['messageIds'].first,
-        content: content,
+        message_id: response_hash["messageIds"].first,
+        content:,
         phone: convict.phone
       )
     else
-      puts "Error when sending SMS: #{response_hash['responseMessage']}"
+      puts "Error when sending SMS: #{response_hash["responseMessage"]}"
     end
   end
 end
--- spec/jobs/sms_sender_job_spec.rb
+++ spec/jobs/sms_sender_job_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SmsSenderJob, type: :job do
-  context 'with a phone number' do
-    let(:convict) { FactoryBot.create :convict, phone: '+33606060607' }
-    let(:content) { 'test SMS' }
+  context "with a phone number" do
+    let(:convict) { FactoryBot.create :convict, phone: "+33606060607" }
+    let(:content) { "test SMS" }
 
-    describe '#perform' do
+    describe "#perform" do
       let(:tested_method) { SmsSenderJob.perform_now(convict, content) }
       let(:adapter_dbl) { instance_double LinkMobilityAdapter, send_sms: true }
 
@@ -14,11 +14,11 @@ RSpec.describe SmsSenderJob, type: :job do
         tested_method
       end
 
-      it 'instantiates LinkMobilityAdapter' do
+      it "instantiates LinkMobilityAdapter" do
         expect(LinkMobilityAdapter).to have_received(:new).once
       end
 
-      it 'send sms' do
+      it "send sms" do
         expect(adapter_dbl).to have_received(:send_sms).once.with(convict, content)
       end
     end
