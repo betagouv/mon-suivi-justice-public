c016c85d5e96b98e8e277abb78f3666c24935104
Charles Marcoin <charles.marcoin@gmail.com>
2022-11-02 22:17:45 +0100

    added spip 61 argentan

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip61_argentan', title: 'Preparer Spip61 Argentan', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_bourges', title: 'Preparer Sap Bourges', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_marseille', title: 'Preparer Sap Marseille', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_boulogne_sur_mer', title: 'Preparer Sap Boulogne Sur Mer', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -168,6 +169,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip61_argentan', title: 'Preparer Spip61 Argentan', view_template: 'preparer_spip61_argentan'},
     {name: 'preparer_sap_bourges', title: 'Preparer Sap Bourges', view_template: 'preparer_sap_bourges'},
     {name: 'preparer_sap_marseille', title: 'Preparer Sap Marseille', view_template: 'preparer_sap_marseille'},
     {name: 'preparer_sap_boulogne_sur_mer', title: 'Preparer Sap Boulogne Sur Mer', view_template: 'preparer_sap_boulogne_sur_mer'},
