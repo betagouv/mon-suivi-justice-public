f8b41f11fe9cca4624bdfb5d4c532f681534c154
Charles Marcoin <charles.marcoin@gmail.com>
2022-11-09 11:01:48 +0100

    added spip 78

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,9 @@ class PagesController < Spina::ApplicationController
 
   include Spina::Api::Paginable
 
+  def preparer_spip78
+  end
+
   def preparer_sap_versailles
   end
 
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe "Pages", type: :request do
     get path
   end
 
+  describe 'GET /preparer_spip78' do
+    let(:path) { preparer_spip78_path }
+
+    it { is_expected.to be_successful }
+  end
+
   describe 'GET /preparer_sap_versailles' do
     let(:path) { preparer_sap_versailles_path }
 
