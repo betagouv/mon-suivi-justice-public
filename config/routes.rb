65591d5bd236f225871ae804b2a2a4a818a35a0c
Charles Marcoin <charles.marcoin@gmail.com>
2022-11-29 14:35:34 +0100

    added spip martinique 972

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip972
     get :preparer_spip91
     get :preparer_spip61_alencon
     get :ma_reinsertion_yvelines
