b1ee4e303ec18d155aad9cd8d9afec588e770add
Charles Marcoin <charles.marcoin@gmail.com>
2022-12-16 11:24:01 +0100

    Updating pages spec file for spip103

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip103' do
+    let(:path) { preparer_spip103_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip71' do
     let(:path) { preparer_spip71_path }
     
