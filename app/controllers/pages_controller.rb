400543ceb6d8d0a23e26543a71a22302e887072f
Charles Marcoin <charles.marcoin@gmail.com>
2022-12-21 11:35:39 +0100

    [skip actions] Updating pages controller file for spip204

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip204
+  end
+
   def preparer_sap_chalons_en_champagne
   end
 
