b7c1f6cab1f1016c4088e471d230aa0846f8bb99
Charles Marcoin <charles.marcoin@gmail.com>
2023-01-03 19:20:19 +0100

    Creation page spip_66 (#92)
    * [skip actions] Adding image for spip_66
    
    * [skip actions] Adding spina view page for for spip_66
    
    * [skip actions] Updating routing file for spip_66
    
    * [skip actions] Updating pages controller file for spip_66
    
    * [skip actions] Updating spina conf file for spip_66
    
    * Updating pages spec file for spip_66
    
    * linting issue

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip_66' do
+    let(:path) { preparer_spip_66_path }
+            
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
+  
   describe 'GET /preparer_spip71_chalon_sur_saone' do
     let(:path) { preparer_spip71_chalon_sur_saone_path }
         
