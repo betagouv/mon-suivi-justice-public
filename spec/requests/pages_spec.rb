ca65e50bcc58a3e59e0ba8c56a85af842a1c1834
Charles Marcoin <charles.marcoin@gmail.com>
2023-01-17 13:58:31 +0100

    Creation page SPIP_53 (#102)
    * [skip actions] Adding image for SPIP_53
    
    * [skip actions] Adding spina view page for for SPIP_53
    
    * [skip actions] Updating routing file for SPIP_53
    
    * [skip actions] Updating pages controller file for SPIP_53
    
    * [skip actions] Updating spina conf file for SPIP_53
    
    * Updating pages spec file for SPIP_53

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_SPIP_53' do
+    let(:path) { preparer_SPIP_53_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip_71_macon' do
     let(:path) { preparer_spip_71_macon_path }
                 
