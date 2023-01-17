933a06d5aa33aeb84fa851b6409f5bd29f80caa0
Charles Marcoin <charles.marcoin@gmail.com>
2023-01-17 15:58:01 +0100

    added ma reinsertion pyrenees orientales

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :ma_reinsertion_pyrenees_orientales
     get :ma_reinsertion_mayenne
     get :preparer_SPIP_53
     get :preparer_spip_71_macon
