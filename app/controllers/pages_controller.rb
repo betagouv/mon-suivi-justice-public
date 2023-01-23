9ec77bef9ac6831f77bb7b16596e57a202792883
Charles Marcoin <charles.marcoin@gmail.com>
2023-01-23 11:54:44 +0100

    [skip actions] Updating pages controller file for spip37

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip37
+  end
+
 
   def ma_reinsertion_pyrenees_orientales
   end
