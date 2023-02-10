6fc3cd74c498f3b58e8dfa9997dc502641b76743
Charles Marcoin <charles.marcoin@gmail.com>
2023-02-10 11:21:10 +0100

    Creation page spip48_lozere (#118)
    * [skip actions] Adding image for spip48_lozere
    
    * [skip actions] Adding spina view page for for spip48_lozere
    
    * [skip actions] Updating routing file for spip48_lozere
    
    * [skip actions] Updating pages controller file for spip48_lozere
    
    * [skip actions] Updating spina conf file for spip48_lozere
    
    * Updating pages spec file for spip48_lozere

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip48_lozere
+  end
+
   def preparer_TJ_bayonne
   end
 
