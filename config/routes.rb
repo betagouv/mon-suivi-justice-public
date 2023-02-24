94d180d38ca7a0b9937f277e426ac815dbd9be59
Charles Marcoin <charles.marcoin@gmail.com>
2023-02-24 15:45:04 +0100

    [skip actions] Updating routing file for spip_51

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip_51
     get :preparer_spip48_lozere
     get :preparer_TJ_bayonne
     get :preparer_TJ_coutances
