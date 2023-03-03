b9832241ffa10c3dd399e360b6a8d2ecf0e1520e
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-03 14:57:35 +0100

    Creation page spip_loiret_montargis (#123)
    * [skip actions] Adding image for spip_loiret_montargis
    
    * [skip actions] Adding spina view page for for spip_loiret_montargis
    
    * [skip actions] Updating routing file for spip_loiret_montargis
    
    * [skip actions] Updating pages controller file for spip_loiret_montargis
    
    * [skip actions] Updating spina conf file for spip_loiret_montargis
    
    * Updating pages spec file for spip_loiret_montargis

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip_loiret_montargis
     get :preparer_spip31_toulouse
     get :preparer_spip31_saint_gaudens
     get :preparer_spip_45
