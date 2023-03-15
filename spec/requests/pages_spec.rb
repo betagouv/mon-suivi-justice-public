241a43451a3cfba4ed9ff642c0c1df701b2c9c21
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-15 15:00:28 +0100

    Creation page preparer_spip_reims (#128)
    * [skip actions] Adding image for preparer_spip_reims
    
    * [skip actions] Adding spina view page for for preparer_spip_reims
    
    * [skip actions] Updating routing file for preparer_spip_reims
    
    * [skip actions] Updating pages controller file for preparer_spip_reims
    
    * [skip actions] Updating spina conf file for preparer_spip_reims
    
    * Updating pages spec file for preparer_spip_reims

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_preparer_spip_reims' do
+    let(:path) { preparer_preparer_spip_reims_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_preparer_sap_gap' do
     let(:path) { preparer_preparer_sap_gap_path }
     
