7dbe70f22ca7a5a72d3bb40533c778cca12c9033
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-15 14:04:59 +0100

    Updating pages spec file for preparer_sap_gap

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_preparer_sap_gap' do
+    let(:path) { preparer_preparer_sap_gap_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_orleans' do
     let(:path) { preparer_sap_orleans_path }
     
