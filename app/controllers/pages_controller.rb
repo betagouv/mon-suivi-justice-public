ece25fef764d83b9a5c4de81061f744fd0bef951
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-24 09:21:53 +0100

    [skip actions] Updating pages controller file for spip41_loir_et_cher

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip41_loir_et_cher
+  end
+
   def preparer_spip95_val_doise
   end
 
