9fe075eb2dec3170a53d5e2a9d58ba9772118122
Charles Marcoin <charles.marcoin@gmail.com>
2023-03-31 10:59:19 +0200

    Updating pages spec file for sap_privas

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_privas' do
+    let(:path) { preparer_sap_privas_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip41_loir_et_cher' do
     let(:path) { preparer_spip41_loir_et_cher_path }
     
