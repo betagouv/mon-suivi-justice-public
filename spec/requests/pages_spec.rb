24733b4526d68dd0d32c385a2356051aa0f20940
Charles Marcoin <charles.marcoin@gmail.com>
2023-04-13 09:40:08 +0200

    Creation page spip09_foix (#136)
    * [skip actions] Adding image for spip09_foix
    
    * [skip actions] Adding spina view page for for spip09_foix
    
    * [skip actions] Updating routing file for spip09_foix
    
    * [skip actions] Updating pages controller file for spip09_foix
    
    * [skip actions] Updating spina conf file for spip09_foix
    
    * Updating pages spec file for spip09_foix

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip09_foix' do
+    let(:path) { preparer_spip09_foix_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip47' do
     let(:path) { preparer_spip47_path }
     
