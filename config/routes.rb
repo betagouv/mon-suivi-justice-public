994e3c75962728f6ae1bf13367666bb4458b76c7
Charles Marcoin <charles.marcoin@gmail.com>
2023-04-14 16:00:04 +0200

    [skip actions] Updating routing file for spip67_saverne

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip67_saverne
     get :preparer_sap_castres
     get :preparer_spip09_foix
     get :preparer_spip47
