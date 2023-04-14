1b4e38251babd1fdc42681bed75026e4f962c861
Charles Marcoin <charles.marcoin@gmail.com>
2023-04-14 08:54:13 +0200

    Updating pages spec file for sap_castres

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_castres' do
+    let(:path) { preparer_sap_castres_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip09_foix' do
     let(:path) { preparer_spip09_foix_path }
     
