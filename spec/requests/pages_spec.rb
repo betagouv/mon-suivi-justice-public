4c7d0dcea00da4644191107182bae09a0fe853ac
Charles Marcoin <charles.marcoin@gmail.com>
2023-04-28 14:49:41 +0200

    Creation page sap_bobigny (#140)
    * [skip actions] Adding image for sap_bobigny
    
    * [skip actions] Adding spina view page for for sap_bobigny
    
    * [skip actions] Updating routing file for sap_bobigny
    
    * [skip actions] Updating pages controller file for sap_bobigny
    
    * [skip actions] Updating spina conf file for sap_bobigny
    
    * Updating pages spec file for sap_bobigny

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_bobigny' do
+    let(:path) { preparer_sap_bobigny_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_clermond_ferrand' do
     let(:path) { preparer_sap_clermond_ferrand_path }
     
