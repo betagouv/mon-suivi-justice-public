d887a45f7a3ac2c83d087e91f7a9010aa685a0d4
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-16 14:35:50 +0200

    Creation page spip90_belfort (#146)
    * [skip actions] Adding image for spip90_belfort
    
    * [skip actions] Adding spina view page for for spip90_belfort
    
    * [skip actions] Updating routing file for spip90_belfort
    
    * [skip actions] Updating pages controller file for spip90_belfort
    
    * [skip actions] Updating spina conf file for spip90_belfort
    
    * Updating pages spec file for spip90_belfort

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip90_belfort
+  end
+
   def preparer_sap82_montauban
   end
 
