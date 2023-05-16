493bbb443bbf2d0d060efdf57dabf490d436e698
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-16 14:48:29 +0200

    [skip actions] Updating pages controller file for spip70_lure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip70_lure
+  end
+
   def preparer_spip90_belfort
   end
 
