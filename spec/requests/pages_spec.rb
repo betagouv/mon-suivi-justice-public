b47f5485a97814abe336606e23e5193937f6ad60
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-16 11:20:53 +0200

    Creation page sap82_montauban (#145)
    * [skip actions] Adding image for sap82_montauban
    
    * [skip actions] Adding spina view page for for sap82_montauban
    
    * [skip actions] Updating routing file for sap82_montauban
    
    * [skip actions] Updating pages controller file for sap82_montauban
    
    * [skip actions] Updating spina conf file for sap82_montauban
    
    * Updating pages spec file for sap82_montauban

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap82_montauban' do
+    let(:path) { preparer_sap82_montauban_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip88_saint_die_des_vosges' do
     let(:path) { preparer_spip88_saint_die_des_vosges_path }
     
