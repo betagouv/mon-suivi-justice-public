89a6f393e73b0eda91be1e238c472b5b2fc3ee85
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-17 15:33:48 +0200

    Creation page sap_la_rochelle (#149)
    * [skip actions] Adding image for sap_la_rochelle
    
    * [skip actions] Adding spina view page for for sap_la_rochelle
    
    * [skip actions] Updating routing file for sap_la_rochelle
    
    * [skip actions] Updating pages controller file for sap_la_rochelle
    
    * [skip actions] Updating spina conf file for sap_la_rochelle
    
    * Updating pages spec file for sap_la_rochelle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_sap_la_rochelle
+  end
+
   def preparer_spip70_vesoul
   end
 
