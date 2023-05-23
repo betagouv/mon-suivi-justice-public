ebf78f41e3822bffab0bc66557a834d816ec8d0d
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-23 10:58:01 +0200

    Updating pages spec file for spip07_privas

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Pages", type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip07_privas' do
+    let(:path) { preparer_spip07_privas_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_albi' do
     let(:path) { preparer_sap_albi_path }
     
