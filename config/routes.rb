766970861a59f4d85b3858b2f3c2f548c6fbc720
Charles Marcoin <charles.marcoin@gmail.com>
2023-05-23 10:57:59 +0200

    [skip actions] Updating routing file for spip07_privas

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip07_privas
     get :preparer_sap_albi
     get :preparer_sap_brive_la_gaillarde
     get :preparer_sap_la_rochelle
