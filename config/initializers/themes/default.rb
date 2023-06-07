2e078afbd89529cae9a44a14a124d6985ff2ec39
Charles Marcoin <charles.marcoin@gmail.com>
2023-06-07 09:49:56 +0200

    Creation page sap_arras (#157)
    * [skip actions] Adding image for sap_arras
    
    * [skip actions] Adding spina view page for for sap_arras
    
    * [skip actions] Updating routing file for sap_arras
    
    * [skip actions] Updating pages controller file for sap_arras
    
    * [skip actions] Updating spina conf file for sap_arras
    
    * Updating pages spec file for sap_arras

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_sap_arras', title: 'Preparer sap_arras', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip76_dieppe', title: 'Preparer spip76_dieppe', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_limoges', title: 'Preparer sap_limoges', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'ma_reinsertion_territoire_de_belfort', title: 'Ma Reinsertion Territoire De Belfort', parts: %w[main_title main_description rich_collapse]},
@@ -233,6 +234,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_sap_arras', title: 'Preparer sap_arras', view_template: 'preparer_sap_arras'},
     {name: 'preparer_spip76_dieppe', title: 'Preparer spip76_dieppe', view_template: 'preparer_spip76_dieppe'},
     {name: 'preparer_sap_limoges', title: 'Preparer sap_limoges', view_template: 'preparer_sap_limoges'},
     {name: 'ma_reinsertion_territoire_de_belfort', title: 'Ma Reinsertion Territoire De Belfort', view_template: 'ma_reinsertion_territoire_de_belfort'},
