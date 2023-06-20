14a862ef0dc70bee960d5bccdb99aad8b291f033
Charles Marcoin <charles.marcoin@gmail.com>
2023-06-20 17:04:30 +0200

    [skip actions] Updating pages controller file for sap_macon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_sap_macon
+  end
+
   def preparer_spip52_chaumont
   end
 
