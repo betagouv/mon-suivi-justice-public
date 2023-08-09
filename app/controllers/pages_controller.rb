0c714bfbfbd31fb137a75f6ee4d89f5e4227173b
Charles Marcoin <charles.marcoin@gmail.com>
2023-08-09 14:03:29 +0200

    Creation page spip06_mougins (#176)
    * [skip actions] Adding image for spip06_mougins
    
    * [skip actions] Adding spina view page for for spip06_mougins
    
    * [skip actions] Updating routing file for spip06_mougins
    
    * [skip actions] Updating pages controller file for spip06_mougins
    
    * [skip actions] Updating spina conf file for spip06_mougins
    
    * Updating pages spec file for spip06_mougins

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip06_mougins
+  end
+
 
   def ma_reinsertion_vaucluse; end
 
