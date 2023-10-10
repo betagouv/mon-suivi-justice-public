a1c9a9f5fca6f4247e184504668e001275cba947
Charles Marcoin <charles.marcoin@gmail.com>
2023-10-10 16:28:23 +0200

    Updating pages spec file for sap_avignon

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_avignon' do
+    let(:path) { preparer_sap_avignon_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip89_auxerre' do
     let(:path) { preparer_spip89_auxerre_path }
     
