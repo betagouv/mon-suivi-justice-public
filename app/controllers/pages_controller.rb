bb93137530ac8164fc9b6d47774e829ab555b3ea
Charles Marcoin <charles.marcoin@gmail.com>
2023-10-13 15:30:47 +0200

    [skip actions] Updating pages controller file for wavestone7

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_wavestone7
+  end
+
   def preparer_sap_avignon
   end
 
