68d956e089fdf8803b9ecfba55fcb13ca0cdc312
Charles Marcoin <charles.marcoin@gmail.com>
2023-10-13 15:48:20 +0200

    [skip actions] Updating routing file for wavestone6.rb

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_wavestone6.rb
     get :preparer_sap_avignon
     get :preparer_spip89_auxerre
     get :preparer_sap_dunkerque
