e9340241d676114f619016dd165e2413121e7090
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-02 15:45:54 +0100

    [skip actions] Updating spina conf file for spip19

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip19', title: 'Preparer spip19', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'ma_reinsertion_tarn', title: 'Ma Reinsertion Tarn', parts: %w[main_title main_description rich_collapse]},
     {name: 'preparer_spip81_castres', title: 'Preparer spip81_castres', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_avignon', title: 'Preparer sap_avignon', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -262,6 +263,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip19', title: 'Preparer spip19', view_template: 'preparer_spip19'},
     {name: 'ma_reinsertion_tarn', title: 'Ma Reinsertion Tarn', view_template: 'ma_reinsertion_tarn'},
     {name: 'preparer_spip81_castres', title: 'Preparer spip81_castres', view_template: 'preparer_spip81_castres'},
     {name: 'preparer_sap_avignon', title: 'Preparer sap_avignon', view_template: 'preparer_sap_avignon'},
