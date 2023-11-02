2a81000f80fd7624b564af481786f6190554894a
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-02 15:45:53 +0100

    [skip actions] Updating routing file for spip19

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip19
     get :ma_reinsertion_tarn
     get :preparer_spip81_castres
     get :preparer_sap_avignon
