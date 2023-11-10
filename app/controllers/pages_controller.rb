478ac251786f8e41b18b289c2b4831c1f6946ab1
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-10 09:59:50 +0100

    [skip actions] Updating pages controller file for spip19_tulle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip19_tulle
+  end
+
   def preparer_sap_brest
   end
 
