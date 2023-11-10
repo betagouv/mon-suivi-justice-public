d1618db3661b1f49927eb958f4f3321dafd078d7
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-10 09:29:26 +0100

    Creation page sap_brest (#223)
    * [skip actions] Adding image for sap_brest
    
    * [skip actions] Adding spina view page for for sap_brest
    
    * [skip actions] Updating routing file for sap_brest
    
    * [skip actions] Updating pages controller file for sap_brest
    
    * [skip actions] Updating spina conf file for sap_brest
    
    * Updating pages spec file for sap_brest

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_brest' do
+    let(:path) { preparer_sap_brest_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip19' do
     let(:path) { preparer_spip19_path }
     
