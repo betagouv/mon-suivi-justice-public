4829c8424f2a50b82e3b169b203f08690d4c871a
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-15 15:20:01 +0100

    Creation page sap_roanne (#227)
    * [skip actions] Adding image for sap_roanne
    
    * [skip actions] Adding spina view page for for sap_roanne
    
    * [skip actions] Updating routing file for sap_roanne
    
    * [skip actions] Updating pages controller file for sap_roanne
    
    * [skip actions] Updating spina conf file for sap_roanne
    
    * Updating pages spec file for sap_roanne

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_roanne' do
+    let(:path) { preparer_sap_roanne_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip19_brive' do
     let(:path) { preparer_spip19_brive_path }
     
