fc23af68b7a72ba51b6264a157d01134bf574501
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-16 15:20:20 +0100

    [skip actions] Updating pages controller file for sap_sarreguemines

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_sap_sarreguemines
+  end
+
   def preparer_sap_bastia
   end
 
