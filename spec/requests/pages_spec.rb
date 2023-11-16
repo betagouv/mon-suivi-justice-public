bd70a18ae363eb88cc17a16bcc5fc92958c74407
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-16 13:58:37 +0100

    Creation page sap_bastia (#230)
    * [skip actions] Adding image for sap_bastia
    
    * [skip actions] Adding spina view page for for sap_bastia
    
    * [skip actions] Updating routing file for sap_bastia
    
    * [skip actions] Updating pages controller file for sap_bastia
    
    * [skip actions] Updating spina conf file for sap_bastia
    
    * Updating pages spec file for sap_bastia

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_bastia' do
+    let(:path) { preparer_sap_bastia_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_saint_quentin' do
     let(:path) { preparer_sap_saint_quentin_path }
     
