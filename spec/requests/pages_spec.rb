15d5a09440d3b1bbd1dabc22d63ee7de885d72d0
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-17 10:45:38 +0100

    Updating pages spec file for sap_epinal

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_epinal' do
+    let(:path) { preparer_sap_epinal_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_tulle' do
     let(:path) { preparer_sap_tulle_path }
     
