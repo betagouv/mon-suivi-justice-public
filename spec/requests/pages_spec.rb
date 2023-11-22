c7cefc2612310b99211590c67d70b111f1980d49
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-22 16:07:43 +0100

    Updating pages spec file for sap_lisieux

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_lisieux' do
+    let(:path) { preparer_sap_lisieux_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_toulouse' do
     let(:path) { preparer_sap_toulouse_path }
     
