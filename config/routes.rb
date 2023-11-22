6ea66a79cc4f5d7932e57f34e95d1c2b34ca69a9
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-22 16:18:08 +0100

    [skip actions] Updating routing file for spip13_tarascon

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip13_tarascon
     get :preparer_sap_lisieux
     get :preparer_sap_toulouse
     get :preparer_spip04
