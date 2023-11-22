11e66633e0b5e5969004e80c56409392ff185c8c
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-22 16:07:41 +0100

    [skip actions] Updating routing file for sap_lisieux

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_sap_lisieux
     get :preparer_sap_toulouse
     get :preparer_spip04
     get :preparer_sap_saint_denis
