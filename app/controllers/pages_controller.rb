47a7e2dceae213779113edfa81f991f382c8d599
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-23 16:50:01 +0100

    [skip actions] Updating pages controller file for spip13_arles_tarascon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip13_arles_tarascon
+  end
+
   def preparer_spip13_tarascon
   end
 
