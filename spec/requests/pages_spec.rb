d57c85aeced3c3e1455517e96d838a9663f09c3a
Charles Marcoin <charles.marcoin@gmail.com>
2023-11-23 16:50:03 +0100

    Updating pages spec file for spip13_arles_tarascon

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip13_arles_tarascon' do
+    let(:path) { preparer_spip13_arles_tarascon_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip13_tarascon' do
     let(:path) { preparer_spip13_tarascon_path }
     
