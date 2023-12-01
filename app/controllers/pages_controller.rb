e4731a888f9ea3fed74dff132dd9aab2564a12c6
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-01 11:31:10 +0100

    [skip actions] Updating pages controller file for sap_montpellier

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_sap_montpellier
+  end
+
   def preparer_spip77_fontainebleau
   end
 
