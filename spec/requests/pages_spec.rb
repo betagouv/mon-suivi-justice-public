ec74ac037beac6d03129bcb936a765b9c2f44fdf
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-01 11:31:12 +0100

    Updating pages spec file for sap_montpellier

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_montpellier' do
+    let(:path) { preparer_sap_montpellier_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip77_fontainebleau' do
     let(:path) { preparer_spip77_fontainebleau_path }
     
