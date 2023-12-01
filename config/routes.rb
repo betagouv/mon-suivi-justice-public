f76d2c9732a7ae81b9cf0f8bd646c44166252733
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-01 08:43:00 +0100

    Creation page sap_vesoul (#249)
    * [skip actions] Adding image for sap_vesoul
    
    * [skip actions] Adding spina view page for for sap_vesoul
    
    * [skip actions] Updating routing file for sap_vesoul
    
    * [skip actions] Updating pages controller file for sap_vesoul
    
    * [skip actions] Updating spina conf file for sap_vesoul
    
    * Updating pages spec file for sap_vesoul

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_sap_vesoul
     get :preparer_spip73_albertville
     get :preparer_sap_dieppe
     get :preparer_sap_chambery
