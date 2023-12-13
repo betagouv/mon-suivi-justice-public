21a54ecad296acb2aff2f909c197acc2ccb8f125
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-13 18:23:57 +0100

    Creation page spip40_mont_de_marsan (#258)
    * [skip actions] Adding image for spip40_mont_de_marsan
    
    * [skip actions] Adding spina view page for for spip40_mont_de_marsan
    
    * [skip actions] Updating routing file for spip40_mont_de_marsan
    
    * [skip actions] Updating pages controller file for spip40_mont_de_marsan
    
    * [skip actions] Updating spina conf file for spip40_mont_de_marsan
    
    * Updating pages spec file for spip40_mont_de_marsan

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip40_mont_de_marsan' do
+    let(:path) { preparer_spip40_mont_de_marsan_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip57_metz' do
     let(:path) { preparer_spip57_metz_path }
         it do
