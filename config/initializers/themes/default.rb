2cde1bcbb599d3be4d51f09582694221850192c4
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-28 10:55:26 +0100

    Creation page spip72 (#278)
    * [skip actions] Adding image for spip72
    
    * [skip actions] Adding spina view page for for spip72
    
    * [skip actions] Updating routing file for spip72
    
    * [skip actions] Updating pages controller file for spip72
    
    * [skip actions] Updating spina conf file for spip72
    
    * Updating pages spec file for spip72

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip72', title: 'Preparer spip72', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip55_bar_le_duc', title: 'Preparer spip55_bar_le_duc', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip73_chambery', title: 'Preparer spip73_chambery', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip40_dax', title: 'Preparer spip40_dax', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -298,6 +299,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip72', title: 'Preparer spip72', view_template: 'preparer_spip72'},
     {name: 'preparer_spip55_bar_le_duc', title: 'Preparer spip55_bar_le_duc', view_template: 'preparer_spip55_bar_le_duc'},
     {name: 'preparer_spip73_chambery', title: 'Preparer spip73_chambery', view_template: 'preparer_spip73_chambery'},
     {name: 'preparer_spip40_dax', title: 'Preparer spip40_dax', view_template: 'preparer_spip40_dax'},
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip72' do
+    let(:path) { preparer_spip72_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip55_bar_le_duc' do
     let(:path) { preparer_spip55_bar_le_duc_path }
     
