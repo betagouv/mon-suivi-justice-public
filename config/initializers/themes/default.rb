118d23744b46d15dd067a95d74a67c2e32c9a2f1
Charles Marcoin <charles.marcoin@gmail.com>
2023-12-29 11:22:19 +0100

    Creation page spip34_montpellier (#281)
    * [skip actions] Adding image for spip34_montpellier
    
    * [skip actions] Adding spina view page for for spip34_montpellier
    
    * [skip actions] Updating routing file for spip34_montpellier
    
    * [skip actions] Updating pages controller file for spip34_montpellier
    
    * [skip actions] Updating spina conf file for spip34_montpellier
    
    * Updating pages spec file for spip34_montpellier

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip34_montpellier', title: 'Preparer spip34_montpellier', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip64_bayonne', title: 'Preparer spip64_bayonne', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip64_pau', title: 'Preparer spip64_pau', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip72', title: 'Preparer spip72', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -301,6 +302,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip34_montpellier', title: 'Preparer spip34_montpellier', view_template: 'preparer_spip34_montpellier'},
     {name: 'preparer_spip64_bayonne', title: 'Preparer spip64_bayonne', view_template: 'preparer_spip64_bayonne'},
     {name: 'preparer_spip64_pau', title: 'Preparer spip64_pau', view_template: 'preparer_spip64_pau'},
     {name: 'preparer_spip72', title: 'Preparer spip72', view_template: 'preparer_spip72'},
