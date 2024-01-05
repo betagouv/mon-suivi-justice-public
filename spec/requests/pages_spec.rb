efb97361e5e2246e46a5e2a2ae707af54c1f54b9
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-05 11:12:03 +0100

    Creation page sap_cayenne (#286)
    * [skip actions] Adding image for sap_cayenne
    
    * [skip actions] Adding spina view page for for sap_cayenne
    
    * [skip actions] Updating routing file for sap_cayenne
    
    * [skip actions] Updating pages controller file for sap_cayenne
    
    * [skip actions] Updating spina conf file for sap_cayenne
    
    * Updating pages spec file for sap_cayenne

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_cayenne' do
+    let(:path) { preparer_sap_cayenne_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_spip34_montpellier' do
     let(:path) { preparer_spip34_montpellier_path }
     
