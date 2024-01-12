476a53738987c45c1a2f7a653bb5df3b67054aed
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-12 11:14:27 +0100

    [skip actions] Updating pages controller file for spip27_evreux

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip27_evreux
+  end
+
   def preparer_spip15_63_clermont_ferrand
   end
 
