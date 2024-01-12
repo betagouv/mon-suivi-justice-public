26ba87fb7043c2e9a3125766cbdd7c87ef00e24a
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-12 10:26:50 +0100

    [skip actions] Updating pages controller file for spip15/63_clermont_ferrand

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip15/63_clermont_ferrand
+  end
+
   def preparer_spip55_verdun
   end
 
