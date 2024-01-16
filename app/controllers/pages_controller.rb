d0e47428afc7b5ca55f56b429c914475ba0b424d
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-16 15:24:43 +0100

    Creation page sap_77_fontainebleau (#298)
    * [skip actions] Adding image for sap_77_fontainebleau
    
    * [skip actions] Adding spina view page for for sap_77_fontainebleau
    
    * [skip actions] Updating routing file for sap_77_fontainebleau
    
    * [skip actions] Updating pages controller file for sap_77_fontainebleau
    
    * [skip actions] Updating spina conf file for sap_77_fontainebleau
    
    * Updating pages spec file for sap_77_fontainebleau

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_sap_77_fontainebleau
+  end
+
   def preparer_spip27_evreux
   end
 
