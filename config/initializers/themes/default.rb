17f3554801604f762685df2c455daace48e75b1f
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-23 17:11:48 +0100

    [skip actions] Updating spina conf file for spip15_63_aurillac

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -4,7 +4,7 @@
 # It's where you define everything that's editable in Spina CMS.
 
 # Zip codes for the "Préparer ma convocation" page select
-ZIP_CODES ||= ["02", "03", "04", "06", "13", "14", "17", "18", "21", "23", "24", "25", "28", "2A", "2B", "32", "33", "36", "37", "38", "44", "47", "49", "51", "52", "54", "56", "57", "58", "59", "61", "62", "67", "68", "69", "71", "72", "74", "75", "77", "78", "79", "83", "91", "92", "93", "95", "972", "85", "86", "40", "41", "66", "53", "64", "48", "50", "08", "45", "31", "05", "07", "09", "81", "63", "88", "82", "90", "70", "19", "76", "87", "89", "10", "22", "84", "29", "42", "30", "27", "97", "73", "34", "55", "973", "80"].freeze
+ZIP_CODES ||= ["02", "03", "04", "06", "13", "14", "17", "18", "21", "23", "24", "25", "28", "2A", "2B", "32", "33", "36", "37", "38", "44", "47", "49", "51", "52", "54", "56", "57", "58", "59", "61", "62", "67", "68", "69", "71", "72", "74", "75", "77", "78", "79", "83", "91", "92", "93", "95", "972", "85", "86", "40", "41", "66", "53", "64", "48", "50", "08", "45", "31", "05", "07", "09", "81", "63", "88", "82", "90", "70", "19", "76", "87", "89", "10", "22", "84", "29", "42", "30", "27", "97", "73", "34", "55", "973", "80", "15_63"].freeze
 
 Spina::Theme.register do |theme|
   # All views are namespaced based on the theme's name
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip15_63_aurillac', title: 'Preparer spip15_63_aurillac', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_77_fontainebleau', title: 'Preparer sap_77_fontainebleau', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip27_evreux', title: 'Preparer spip27_evreux', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip15_63_clermont_ferrand', title: 'Preparer spip15_63_clermont_ferrand', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -308,6 +309,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip15_63_aurillac', title: 'Preparer spip15_63_aurillac', view_template: 'preparer_spip15_63_aurillac'},
     {name: 'preparer_sap_77_fontainebleau', title: 'Preparer sap_77_fontainebleau', view_template: 'preparer_sap_77_fontainebleau'},
     {name: 'preparer_spip27_evreux', title: 'Preparer spip27_evreux', view_template: 'preparer_spip27_evreux'},
     {name: 'preparer_spip15_63_clermont_ferrand', title: 'Preparer spip15_63_clermont_ferrand', view_template: 'preparer_spip15_63_clermont_ferrand'},
