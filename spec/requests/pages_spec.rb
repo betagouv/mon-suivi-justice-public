b4c268455f2fc86953d4a4892c06ace03b278de6
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-23 17:43:15 +0100

    Creation page spip15_63_aurillac (#301)
    * [skip actions] Adding image for spip15_63_aurillac
    
    * [skip actions] Adding spina view page for for spip15_63_aurillac
    
    * [skip actions] Updating routing file for spip15_63_aurillac
    
    * [skip actions] Updating pages controller file for spip15_63_aurillac
    
    * [skip actions] Updating spina conf file for spip15_63_aurillac
    
    * Updating pages spec file for spip15_63_aurillac

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_spip15_63_aurillac' do
+    let(:path) { preparer_spip15_63_aurillac_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_77_fontainebleau' do
     let(:path) { preparer_sap_77_fontainebleau_path }
     
