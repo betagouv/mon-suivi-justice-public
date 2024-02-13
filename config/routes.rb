51f9fd8a13ef6edb613e95641df75e5e3067e4a7
Charles Marcoin <charles.marcoin@gmail.com>
2024-02-13 14:50:20 +0100

    [skip actions] Updating routing file for spip38_bourgoin_jailleu

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   post :sms_webhook, to: "convicts/sms_webhooks#receive"
 
   scope controller: :pages do
+    get :preparer_spip38_bourgoin_jailleu
     get :preparer_spip76_rouen
     get :preparer_spip57_thionville
     get :preparer_spip68_mulhouse
