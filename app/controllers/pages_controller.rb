6c348a7d5660c66d163542ff351ac85d33be943a
Charles Marcoin <charles.marcoin@gmail.com>
2024-02-15 10:52:24 +0100

    [skip actions] Updating pages controller file for sap_tarbes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_sap_tarbes
+  end
+
 
   def ma_reinsertion_moselle
   end
