f328304442824860f9ce0ca0c41b894af715bf0b
Damien Le Thiec <damien@dlet.fr>
2024-03-05 16:18:44 +0100

    [skip actions] Updating spina conf file for spip33_libourne

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip33_libourne', title: 'Preparer spip33_libourne', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip33_bordeaux', title: 'Preparer spip33_bordeaux', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip14_caen', title: 'Preparer spip14_caen', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip11_carcassonne', title: 'Preparer spip11_carcassonne', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -321,6 +322,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip33_libourne', title: 'Preparer spip33_libourne', view_template: 'preparer_spip33_libourne'},
     {name: 'preparer_spip33_bordeaux', title: 'Preparer spip33_bordeaux', view_template: 'preparer_spip33_bordeaux'},
     {name: 'preparer_spip14_caen', title: 'Preparer spip14_caen', view_template: 'preparer_spip14_caen'},
     {name: 'preparer_spip11_carcassonne', title: 'Preparer spip11_carcassonne', view_template: 'preparer_spip11_carcassonne'},
