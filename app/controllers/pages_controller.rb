d93788608ed56d7821007a4739f561d9e087d676
Damien Le Thiec <damien@dlet.fr>
2024-03-05 16:15:00 +0100

    [skip actions] Updating pages controller file for spip33_bordeaux

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip33_bordeaux
+  end
+
   def preparer_spip14_caen
   end
 
