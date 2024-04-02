38b43f439a41ae5d64646b4f250f4bbc9341b443
Damien Le Thiec <damien@dlet.fr>
2024-04-02 10:18:23 +0200

    Creation page sap-evry (#353)
    * [skip actions] Adding image for sap-evry
    
    * [skip actions] Adding spina view page for for sap-evry
    
    * [skip actions] Updating routing file for sap-evry
    
    * [skip actions] Updating pages controller file for sap-evry
    
    * [skip actions] Updating spina conf file for sap-evry
    
    * Updating pages spec file for sap-evry
    
    * refactor
    
    ---------
    
    Co-authored-by: Matthieu Faugere <matthieu.faugere@gmail.com>

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'Pages', type: :request do
     FactoryBot.create(:account)
   end
 
+  describe 'GET /preparer_sap_evry' do
+    let(:path) { preparer_sap_evry_path }
+    
+    it do
+      get path
+      is_expected.to be_successful
+    end
+  end
   describe 'GET /preparer_sap_bourg_en_bresse' do
     let(:path) { preparer_sap_bourg_en_bresse_path }
     
