8aa8c74f32d5f1ab440ce6566cee4e63b311bc87
Damien Le Thiec <damien@dlet.fr>
2024-04-17 11:13:07 +0200

    [skip actions] Updating spina conf file for spip65_tarbes

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip65_tarbes', title: 'Preparer spip65_tarbes', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip14_lisieux', title: 'Preparer spip14_lisieux', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_sap_pau', title: 'Preparer sap_pau', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip29_morlaix', title: 'Preparer spip29_morlaix', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -339,6 +340,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip65_tarbes', title: 'Preparer spip65_tarbes', view_template: 'preparer_spip65_tarbes'},
     {name: 'preparer_spip14_lisieux', title: 'Preparer spip14_lisieux', view_template: 'preparer_spip14_lisieux'},
     {name: 'preparer_sap_pau', title: 'Preparer sap_pau', view_template: 'preparer_sap_pau'},
     {name: 'preparer_spip29_morlaix', title: 'Preparer spip29_morlaix', view_template: 'preparer_spip29_morlaix'},
