506091cb29620714bedd52277a7bea70d0e4255a
Damien Le Thiec <damien@dlet.fr>
2024-07-09 14:16:20 +0200

    Add page ma réinsertion seine maritine

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root "pages#landing"
 
   scope controller: :pages do
+    get :ma_reinsertion_seine_maritine
     get :preparer_spip62_bethune
     get :preparer_spip27_bernay
     get :preparer_sap_saint_brieuc
