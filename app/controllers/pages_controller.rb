95e589acc522fe37df8203a1c9c6187a7b884cb9
Damien Le Thiec <damien@dlet.fr>
2024-07-23 09:58:56 +0200

    [skip actions] Updating pages controller file for spip60_beauvais

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip60_beauvais
+  end
+
 
   def ma_reinsertion_seine_maritine
   end
