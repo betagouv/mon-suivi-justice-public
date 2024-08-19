31560454550736c968c7be85b84a35971d7877e6
Damien Le Thiec <damien@dlet.fr>
2024-08-19 21:41:12 +0200

    [skip actions] Updating pages controller file for spip13_aix_en_provence

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip13_aix_en_provence
+  end
+
 
   def ma_reinsertion_maine_et_loire
   end
