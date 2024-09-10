99b707791ff5c89fd3988e2d04be99926a03d126
Damien Le Thiec <damien@dlet.fr>
2024-09-10 11:58:48 +0200

    [skip actions] Updating routing file for sap_carcassonne

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root "pages#landing"
 
   scope controller: :pages do
+    get :preparer_sap_carcassonne
     get :preparer_sap_valenciennes
     get :preparer_spip68_colmar
     get :preparer_spip13_aix_en_provence
