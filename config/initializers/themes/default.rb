62365616cb1d766ba008e01486773500fe69703c
Damien Le Thiec <damien@dlet.fr>
2024-10-18 11:57:36 +0200

    [skip actions] Updating spina conf file for spip74_bonneville

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -66,6 +66,7 @@ Spina::Theme.register do |theme|
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
   theme.view_templates = [
+    {name: 'preparer_spip74_bonneville', title: 'Preparer spip74_bonneville', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip01_bourg_en_bresse', title: 'Preparer spip01_bourg_en_bresse', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip79_annemasse', title: 'Preparer spip79_annemasse', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
     {name: 'preparer_spip59_cambrai', title: 'Preparer spip59_cambrai', parts: %w[main_title main_description zip_code_select direction_collapse_title direction_collapse_first_rich_content direction_collapse_second_rich_content collapse_rich_content_good_practice collapse_rich_content_alert direction_collapse_button_text direction_collapse_button_link rich_collapse]},
@@ -364,6 +365,7 @@ Spina::Theme.register do |theme|
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
+    {name: 'preparer_spip74_bonneville', title: 'Preparer spip74_bonneville', view_template: 'preparer_spip74_bonneville'},
     {name: 'preparer_spip01_bourg_en_bresse', title: 'Preparer spip01_bourg_en_bresse', view_template: 'preparer_spip01_bourg_en_bresse'},
     {name: 'preparer_spip79_annemasse', title: 'Preparer spip79_annemasse', view_template: 'preparer_spip79_annemasse'},
     {name: 'preparer_spip59_cambrai', title: 'Preparer spip59_cambrai', view_template: 'preparer_spip59_cambrai'},
