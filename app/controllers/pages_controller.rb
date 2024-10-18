cb7af585a87183e297a75848584de9b00c21c871
Damien Le Thiec <damien@dlet.fr>
2024-10-18 11:57:35 +0200

    [skip actions] Updating pages controller file for spip74_bonneville

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,9 @@ class PagesController < Spina::ApplicationController
   # Spina::Navigation.find_by(name: "main").pages.pluck(:name) pour la navigation
 
   include Spina::Api::Paginable
+  def preparer_spip74_bonneville
+  end
+
   def preparer_spip01_bourg_en_bresse
   end
 
